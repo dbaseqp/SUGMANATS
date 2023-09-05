8e2ac20b1d7b7dba47bcc126250be059058ff7a2
dbaseqp <gabrielfok128@gmail.com>
2023-09-05 02:32:14 -0700

    UI draft

--- routes.go
+++ routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"bytes"
+	"github.com/BurntSushi/toml"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +15,10 @@ func addPublicRoutes(g *gin.RouterGroup) {
 }
 
 func addPrivateRoutes(g *gin.RouterGroup) {
+	g.GET("/about", viewAbout)
 	g.GET("/logout", logout)
 	g.GET("/dashboard", viewDashboard)
+	g.GET("/boxes", viewBoxes)
 }
 
 func pageData(c *gin.Context, title string, ginMap gin.H) gin.H {
@@ -23,13 +27,19 @@ func pageData(c *gin.Context, title string, ginMap gin.H) gin.H {
 	newGinMap["user"] = getUser(c)
 	newGinMap["config"] = tomlConf
 	newGinMap["operation"] = tomlConf.Operation
+	// newGinMap["boxes"] = boxes
 	for key, value := range ginMap {
 		newGinMap[key] = value
 	}
 	return newGinMap
 }
 
+// public routes
+
 func viewIndex (c *gin.Context) {
+	if !getUser(c).IsValid() {
+		c.Redirect(http.StatusSeeOther, "/login")
+	}
 	c.HTML(http.StatusOK, "index.html", pageData(c, "SUGMANATS", nil))
 }
 
@@ -40,6 +50,21 @@ func viewLogin (c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", pageData(c, "Login", nil))
 }
 
+// private routes
+
 func viewDashboard (c *gin.Context) {
 	c.HTML(http.StatusOK, "dashboard.html", pageData(c, "Dashboard", nil))
+}
+
+func viewBoxes (c *gin.Context) {
+	c.HTML(http.StatusOK, "boxes.html", pageData(c, "Boxes", nil))
+}
+
+func viewAbout (c *gin.Context) {
+	buf := new(bytes.Buffer)
+	if err := toml.NewEncoder(buf).Encode(tomlConf); err != nil {
+		c.HTML(http.StatusInternalServerError, "settings.html", pageData(c, "Settings", gin.H{"error": err}))
+		return
+	}
+	c.HTML(http.StatusOK, "about.html", pageData(c, "About", gin.H{"config": buf.String()}))
 }
